13de1770edc8caa4e9090c4269c28b507249de30
Matin297 <52866914+Matin297@users.noreply.github.com>
2019-11-15 18:48:03 +0330

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouters from './routes/AppRouter'
 import configStore from './configStore/configStore'
-import { addExpense } from './actions/expenses.js'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 200 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 10111, createdAt: -2000000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 102000, createdAt: 1000 }))
+console.log('from app.js');
 
 ReactDOM.render(
     <Provider store={store}>
